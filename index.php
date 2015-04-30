a507e2db5f8a109a569b30c926b4edea0950faca
wael <wael54@hotmail.com>
2015-04-30 20:39:15 +0200

    updated index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 require_once('connexion.php'); // Objet PDO
@@ -39,47 +38,6 @@ require_once('connexion.php'); // Objet PDO
             </form>
             
         </div> <!--/container -->
-    </body>
-</html>
-<?php
-session_start();
-?>
-
-<!DOCTYPE html>
-<html lang="fr-FR">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <meta name="description" content="">
-        <meta name="author" content="">
-
-        <title>Projet_TER</title>
-
-        <!-- Bootstrap core CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    </head>
-
-    <body>
-
-        <div class="container">
-            <div class="jumbotron">
-                <div class="container">
-                    <h1 class="text-center"><a href="index.php">Analyse SÃ©mantique et Langue naturelle</a></h1>
-                </div>
-            </div>
-
-            <form class="text-center" action="traitement.php" method="post">    
-                <br>
-                <label for="inputText" class="sr-only">Texte Ã  analyser </label>
-                <input type="text" name="text" class="form-control" placeholder="Tapez votre texte" required autofocus>
-                <br>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Analyser la phrase</button>
-            </form>
-       
-        </div> <!-- /container -->
     </body>
 </html>
 
