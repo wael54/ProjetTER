6d9855d6af8419cbe7371cb29f438bf247053852
Lucien Kerisit <lucien.kerisit@etu.univ-amu.fr>
2015-05-27 19:42:00 +0200

    Commentaires
    Ajout de commentaires

--- getWord.php
+++ getWord.php
@@ -1,4 +1,8 @@
 <?php
+	/* Fichier appele uniquement par un appel ajax depuis traitement.php 
+	** Renvoie les details de l'annotation sur le mot d'ID en parametre
+	*/
+	
 	set_include_path('.');
     require_once('./include/include.php');
     
